a61ab8ac4538fef4e10d9fc805e6e4ca1c6c8fa5
Sam Seaver <seaver@anl.gov>
2026-02-05 23:38:52 -0600

    Reducing code to simplify process

--- generate_reaction_scores.py
+++ generate_reaction_scores.py
@@ -5,36 +5,14 @@ project_root = Path(__file__).resolve().parent
 module = str(project_root)+'/src/reaction_scores'
 if module not in sys.path:
     sys.path.append(module)
-module = str(project_root)+'/src/plastidial_model_extraction'
-if module not in sys.path:
-    sys.path.append(module)
-
-# plastidial model 
-from src.plastidial_model_extraction.plastidial_model_generator import ModelBuilder, ModelGenerator
-
-# Reaction scores 
-import src.reaction_scores.computeScoresAndPredictions as csp
 
 # Parameters class
 # The parameters dictate where to find the files
 # And how to link them to different species/conditions
-from src.util.parameters import Parameters_QPSI
-from src.util.parameters import Parameters_ColdResponse
-from src.util.parameters import Parameters_BRaVE
-project_param = Parameters_BRaVE()
+from src.util.parameters import Parameters
+project_param = Parameters()
 
-# This flag is for the sake of our publication
-# As described in our work, we take a full reconstruction
-# And we reduce it to a working reconstruction of a plastid
-compute_scores = True
-
-if project_param.generate_plastidial_models:
-    spcs = ['Athaliana', 'Poplar', 'Sorghum']
-    for spc in spcs:
-        mBuilder = ModelBuilder(spc)
-        mGen = ModelGenerator(mBuilder)
-        mGen.run_model_generator(clean_up=True)
-
-# Compute reaction scores
-if compute_scores:
-    csp.generate_reactionScores(project_param,project_species=['Sorghum'])
+# Reaction scores 
+import src.reaction_scores.computeScoresAndPredictions as csp
+for species in project_param.project_species:
+    csp.generate_reactionScores(project_param,project_species=[species])
